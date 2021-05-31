01a425fa2d10bed979351407d83a142a017ac395
Vivek <viveksonybcd@gmail.com>
2021-05-31 18:59:41 +0530

    added tasks and task-item components

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,45 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Buy Meds',
+    day: 'May 4th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Clean Your room',
+    day: 'May 3th at 4:30pm',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Pay bills',
+    day: 'May 1st at 12:10pm',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Meeting at school',
+    day: 'May 7th at 10:00am',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Buy Meds',
+    day: 'May 4th at 2:30pm',
+    reminder: false,
+  },
+  {
+    id: 6,
+    text: 'Buy Meds',
+    day: 'May 4th at 2:30pm',
+    reminder: false,
+  },
+  {
+    id: 7,
+    text: 'Buy Meds',
+    day: 'May 4th at 2:30pm',
+    reminder: true,
+  },
+];
